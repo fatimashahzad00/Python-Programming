b6d345a22ab8928acb28f3a991a26c995c290ed9
Fatima Shahzad <107702644+fatimashahzad00@users.noreply.github.com>
2024-08-17 08:10:51 +0000

    variables booleans and strings

---
+++ Variables.py
@@ -0,0 +1,20 @@
+# Variables Boolean and Strings
+is_game_over = False
+is_game_over = True
+
+print(is_game_over)
+print(type(is_game_over))
+
+is_game_over = 1
+print(is_game_over)
+print(type(is_game_over))
+
+name = 'Nimish'
+is_game_over_text = "False"
+age_as_a_string = "5"
+print(name)
+print(type(name))
+
+age = 27
+name_and_age = "Nimish: {}".format(age)
+print(name_and_age)
