e01717f9eb19d58abd0b2e47c6f3172116b1feb2
Fatima Shahzad <107702644+fatimashahzad00@users.noreply.github.com>
2024-08-20 13:40:49 +0000

    arithmetic operatore

--- Variables.py
+++ Variables.py
@@ -18,3 +18,21 @@ print(type(name))
 age = 27
 name_and_age = "Nimish: {}".format(age)
 print(name_and_age)
+
+# Variables - Integers and Floats
+
+x_position = 10
+print(x_position)
+
+x_position = 15
+print(x_position)
+
+pi = 3.14
+pi = 3.14159
+print(pi)
+
+
+x_position = 15.0
+print(x_position)
+
+print(type(x_position))
---
+++ arithmetic_operators.py
@@ -0,0 +1,22 @@
+x_position = 10
+forward = x_position + 1
+print(forward)
+
+backward = x_position - 1
+print(backward)
+
+reminder = 5 %2
+print(reminder)
+
+floor_division = 5 // 2
+print(floor_division)
+
+five_squared = 5 ** 2
+print(five_squared)
+
+x_position += 1
+print(x_position)
+
+first_name = "Fatima "
+last_name = "Shahzad"
+print(first_name + last_name)
