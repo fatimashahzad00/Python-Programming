1d59fdc09cb12db129915a0dc2718b019ebefba2
Fatima Shahzad <fatimashahzad062@gmail.com>
2024-08-25 20:29:43 +0500

    tuples

---
+++ Components of Python/tuples.py
@@ -0,0 +1,17 @@
+high_score = ("Nimish", 120)
+print(high_score)
+
+high_score = ("Holly", 150)
+print(high_score)
+
+name = high_score[0]
+print(name)
+
+print(len(high_score))
+
+print("Holly" in high_score)
+
+print(name[0])
+print(name[0:2])
+print("Hol" in high_score)
+print(len(name))
